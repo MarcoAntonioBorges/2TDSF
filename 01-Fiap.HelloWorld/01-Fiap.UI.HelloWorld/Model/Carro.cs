f5005226f4d9c1578a5c533845aa665017d014db
Logon PF Fiap <logonpflocal@fiap.com.br>
2019-08-05 09:41:42 -0300

    Introdução ao C#

---
+++ 01-Fiap.HelloWorld/01-Fiap.UI.HelloWorld/Model/Carro.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _01_Fiap.UI.HelloWorld.Model
+{
+    class Carro : Veiculo
+    {
+        //Propriedades (gets e sets)
+        public int Portas { get; set; }
+        public decimal Valor { get; set; }
+
+    }
+}
---
+++ 01-Fiap.HelloWorld/01-Fiap.UI.HelloWorld/Program.cs
@@ -0,0 +1,18 @@
+﻿using _01_Fiap.UI.HelloWorld.Model;
+using System;
+
+namespace Fiap.UI.HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instanciar um carro
+            Carro carro = new Carro();
+            //Atribuir um código ao carro
+            carro.Codigo = 10;
+            //Exibir o código do carro
+            Console.WriteLine(carro.Codigo);            
+        }
+    }
+}
